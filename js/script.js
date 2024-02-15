1c951d0a9d4bf1fe41e9085a9841e34a29624e98
kapil <kapil@vega6.com>
2024-02-15 16:06:40 +0530

    update project

---
+++ js/script.js
@@ -0,0 +1,67 @@
+let navbar = document.querySelector(".wrap-1 #navbar");
+document.querySelector('#menu-btn').addEventListener("click", function (e) {
+    e.preventDefault()
+    navbar.style.left = "0";
+});
+document.querySelector('#close').addEventListener("click", function () {
+    e.preventDefault()
+    navbar.style.left = "-170%";
+    navbar.style.transition = "all 0.4s ease-in";
+});
+$(document).ready(function () {
+    $(".accord-header").click(function (e) {
+        e.preventDefault()
+        if ($(this).next(".accord-p").hasClass("active")) {
+            $(this).next(".accord-p").removeClass("active").slideUp()
+            $(this).find("a").removeClass("fa-minus").addClass("fa-plus")
+        }
+        else {
+            $(".accord .accord-p").removeClass("active").slideUp()
+            $(".accord .accord-header a").removeClass("fa-minus").addClass("fa-plus");
+            $(this).next(".accord-p").addClass("active").slideDown()
+            $(this).find("a").removeClass("fa-plus").addClass("fa-minus")
+        }
+    })
+})
+// $(".accord-header").click(function (e) {
+//     e.preventDefault()
+
+
+//     if ($(this).hasClass("active")) {
+
+//         $(this).removeClass("active");
+//         $(this).next(".accord-p").slideUp();
+//         $(this).find("a.fa-plus").removeClass("fa-plus").addClass("fa-minus")
+//     } else {
+//         $(".accord .accord-p").slideUp()
+//         $(".accord .accord-header a").removeClass("fa-minus").addClass("fa-plus");
+//         $(this).addClass("active")
+//         $(this).next(".accord-p").slideDown()
+//         // $(this).find("a.fa-plus").removeClass("fa-plus").addClass("fa-minus")
+//     }
+
+// });
+
+$('.owl-carousel').owlCarousel({
+
+    loop: true,
+    nav: false,
+    autoplay: true,
+    autoplaytimeout: 700,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        }
+    }
+});
+
+const videoPlay = document.querySelector("#playVideo");
+const videoCont = document.querySelector("#videoCont");
+const videoRem = document.querySelector(".video_wrap");
+videoPlay.addEventListener("click", () => (
+    videoCont.classList.toggle('active-Btn')
+))
+videoRem.addEventListener("click", () => (
+    videoCont.classList.remove('active-Btn')
+))
